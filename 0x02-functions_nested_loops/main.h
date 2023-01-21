8ea2ebd11a88010451f88b6d54464034fad44a25
profech <123248480+profech@users.noreply.github.com>
2023-01-21 14:35:34 +0100

    Add files via upload

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,122 @@
+#ifndef MAIN_H_INCLUDED
+
+#define MAIN_H_INCLUDED
+
+/**
+
+ * _putchar - writes a single character to the standard output stream, stdout.
+
+ *  Return:  returns 1 or 0
+
+ */
+
+int _putchar(char c);
+  
+/**
+
+ * print_alphabet - This Function prints the alphabet, in lowercase, followed by a new line.
+
+ * 
+
+ */
+
+void print_alphabet(void);
+
+
+/**
+
+ * print_alphabet_x10 - prints alphabet 10 times
+
+ * 
+
+ */
+
+void print_alphabet_x10(void);
+
+  
+/**
+
+ * _islower - function checks for lowercase character
+
+ * @c: character used
+
+ * Return: 1 or 0
+
+ */
+
+int _islower(int c);
+
+/**
+
+ * _isalpha - checks if character is alphabet
+
+ * @c: character to use
+
+ * Return: 1 or 0
+
+ */
+
+int _isalpha(int c);
+/**
+
+ * print_sign - prints a sign based on n
+
+ * @n: integer to use
+
+ * Return: 1 or 0  or -1
+
+ */
+
+int print_sign(int n);
+
+/**
+
+ * _abs - gives absolute value
+
+ * @int: integer
+
+ * Return: integer or abs value of int
+
+ */
+
+int _abs(int);
+
+/**
+
+ * print_last_last - prints digit last
+
+ *
+
+ * @int: integer to input
+
+ * Return: last digit of integer
+
+ */
+
+int print_last_digit(int);
+
+
+/**
+
+ * jack_bauer - jack bauer gooo
+
+ *
+
+ */
+
+void jack_bauer(void);
+
+/**
+
+ * times_table - times this up
+
+ *
+
+ *
+
+ */
+
+void times_table(void);
+
+
+#endif
---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,167 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+
+/**
+
+ * _isupper - determines uppercase or not
+
+ * @c: character to determine
+
+ * Return: true or false
+
+ */
+
+int _isupper(int c);
+
+
+
+
+/**
+
+ * _isdigit - checks if it is digit
+
+ * @c: character to check
+
+ *
+
+ * Return: returns boolean
+
+ */
+
+int _isdigit(int c);
+
+
+
+/**
+
+ * mul - multiplies integers
+
+ * @a: integer to multiply
+
+ * @b: integer to multiply
+
+ * Return: gives result
+
+ */
+
+int mul(int a, int b);
+
+/**
+
+ * print_numbers - prints the numbers (0-9)
+
+ *
+
+ */
+
+void print_numbers(void);
+
+/**
+
+ * print_most_numbers - prints the numbers (0-9, except 2,4)
+
+ *
+
+ */
+
+
+
+/**
+
+ * _putchar - writes the character c to std
+
+ * @c: parameter to compare
+
+ * Return: 1 or 0
+
+ */
+
+int _putchar(char c);
+
+
+
+void print_most_numbers(void);
+
+
+
+
+
+/**
+
+ * more_numbers - prints the numbers
+
+ *
+
+ */
+
+void more_numbers(void);
+
+
+/**
+
+ * print_line - prints a line
+
+ *
+
+ * @n: integer to print stuff
+
+ */
+
+void print_line(int n);
+
+
+/**
+
+ * print_diagonal - prints diagonal
+
+ *
+
+ * @n: integer to see how many stupid lines
+
+ */
+
+void print_diagonal(int n);
+
+
+
+/**
+
+ * print_square - prints a square
+
+ *
+
+ * @size: prints the size of whatever
+
+ */
+
+void print_square(int size);
+
+
+
+
+/**
+
+ * print_triangle - prints triangle
+
+ * @size: size of triangle
+
+ */
+
+void print_triangle(int size);
+
+/**
+
+ * print_number - prints number
+
+ *
+
+ * @n: integer to print to char
+
+ */
+
+void print_number(int n);
+
+
+#endif
+
